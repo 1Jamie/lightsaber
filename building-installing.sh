7c71d8e1ecdba060848a9e48aadd3371a7591997
charja13@live.com <charja13@live.com>
2015-06-25 22:14:08 -0700

    added all the new files and tools for new contributors and added the baseimage in for ease of access

---
+++ building-installing.sh
@@ -0,0 +1,4 @@
+##this is not complex at all or imperative, just keeping it uber simple for new people in the spark area
+make install
+make sync
+GAIA_DEV_PIXELS_PER_PX=2.25 DEVICE_DEBUG=1 make reset-gaia
---
+++ settingup_computer_for_spark.sh
@@ -0,0 +1,8 @@
+sudo apt-get install nodejs-legacy
+sudo npm install -g bower 
+sudo npm install -g gulp
+sudo npm install -g apm 
+sudo npm install -g grunt-cli 
+sudo npm install -g browserify
+
+
